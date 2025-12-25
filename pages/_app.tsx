6955da5ea063fec6590fcd743aec622a43798daf
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-12-25 18:07:05 +0100

    import auth provider from auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,15 @@ import type { AppProps } from "next/app";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Analytics } from '@vercel/analytics/react';
+import { AuthProvider } from '@/lib/auth';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <AuthProvider>
       <Component {...pageProps} />
       <Analytics />
       <ToastContainer position="bottom-right" theme="dark" />
-    </>
+    </AuthProvider>
   );
 }
